1480d39d2515cc1eeeee7ce392f03b1c61a4602a
AiODillen <dillen@aio.rest>
2025-04-10 11:43:50 +0000

    Update

--- backup.sh
+++ backup.sh
@@ -1,4 +1,11 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+
+
 rsync -a /home/dillen/.local/share/Steam/steamapps/compatdata/2323157508/pfx/drive_c/users/steamuser/AppData/Roaming/EldenRing/76561198072305731/ /home/dillen/Documents/saves/Elden_Ring/
-git add *
-git commit -m "Update"
+
+
+
+git add -A
+git commit -am "Update"
 git push
