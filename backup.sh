2adef1f7be79415e2bd83b7704b23351e0cac651
AiODillen <dillen@aio.rest>
2025-04-10 12:00:00 +0000

    Update {}

--- backup.sh
+++ backup.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-
+timestamp() {
+  date +"%T" # current time
+}
 
 rsync -a /home/dillen/.local/share/Steam/steamapps/compatdata/2323157508/pfx/drive_c/users/steamuser/AppData/Roaming/EldenRing/76561198072305731/ /home/dillen/Documents/saves/Elden_Ring/
 
 
 
 git add -A
-git commit -am "Update"
+git commit -am "Update {$timestamp}"
 git push
